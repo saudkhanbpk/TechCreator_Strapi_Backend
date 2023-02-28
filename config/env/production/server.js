e699585e896c48f01411bb3d8425bc0cc46e3f90
Abdul Basit <proabdulbasit.me@gmail.com>
2023-03-01 00:23:47 +0500

    dynomic blog comp

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,13 +1,3 @@
-// // Path: ./config/env/production/server.js
-
-// module.exports = ({ env }) => ({
-//   proxy: true,
-//   url: env("MY_HEROKU_URL"), // Sets the public URL of the application.
-//   app: {
-//     keys: env.array("APP_KEYS"),
-//   },
-// });
-
 module.exports = ({ env }) => ({
   url: env("MY_HEROKU_URL"),
 });
--- config/plugins.js
+++ config/plugins.js
@@ -1,21 +1,3 @@
-// module.exports = ({ env }) => ({
-//   upload: {
-//     config: {
-//       provider: "cloudinary",
-//       providerOptions: {
-//         cloud_name: env("CLOUDINARY_NAME"),
-//         api_key: env("CLOUDINARY_KEY"),
-//         api_secret: env("CLOUDINARY_SECRET"),
-//       },
-//       actionOptions: {
-//         upload: {},
-//         uploadStream: {},
-//         delete: {},
-//       },
-//     },
-//   },
-// });
-
 module.exports = ({ env }) => ({
   upload: {
     config: {
